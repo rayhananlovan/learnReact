0b024523846e77a5e8a7a8aec2be430068e43199
Rayhan Nurcholis <rayhan.a.nurcholis@gmail.com>
2023-07-12 08:23:07 +0700

    list-contact

--- src/Home.js
+++ src/Home.js
@@ -1,54 +1,21 @@
 import { useState } from "react";
 const Home = () => {
-  const [person, setPerson] = useState({
-    firstname: "Rayhan",
-    lastname: "Nurcholis",
-    age: "16",
-    address: {
-      address1: "surakarta",
-      address2: "boyolali",
-    },
-  });
-
-  const handleUpdateAge = () => {
-    setPerson({
-      firstname: "Name",
-      lastname: "Name",
-      age: "20",
-    });
-  };
-
-  const handleUpdateAddress = () => {
-    setPerson({
-      firstname: "Name",
-      lastname: "Name",
-      age: "22",
-      address: {
-        firstname: "Rayhan",
-        lastname: "Nurcholis",
-        age: "16",
-        address: {
-          address1: "surakarta",
-          address2: "boyolali",
-          address2: "surabaya",
-        },
-      },
-    });
-  };
-
+  const [contacts, setContacts] = useState([
+    { id: 1, name: "Messi", number: "085627871665" },
+    { id: 2, name: "Murdik", number: "081627779835" },
+    { id: 3, name: "Cucur", number: "087633899273" },
+  ]);
   return (
     <div className="home">
       <h3>Contact List</h3>
-      <div>Firstname: {person.firstname}</div>
-      <div>Lastname: {person.lastname}</div>
-      <div>Age: {person.age}</div>
-      <div>Address: {person.adddress}</div>
-      <ul>
-        <li>{person.address.address1}</li>
-        <li>{person.address.address2}</li>
-      </ul>
-      <button onClick={handleUpdateAge}>Update Age</button>
-      <button onClick={handleUpdateAddress}>Update Address</button>
+      {contacts.map((contact) => (
+        <div className="contact" key={contact.id}>
+          <div className="contact-info">
+            <p className="contact-name">{contact.name}</p>
+            <p>{contact.number} </p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
